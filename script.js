76b7c0c76e2210e819de8074972b6f4327f6432d
Parth Chauhan <parth.chauhan1@gmail.com>
2023-06-17 22:18:11 +0530

    lock unlock and ticket priority color change added

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ let priority_selector=document.querySelector(".priority-selector");
 let textarea_cont=document.querySelector(".textarea-cont");
 let main_cont=document.querySelector(".main-cont");
 let delete_btn=document.querySelector(".remove-btn");
+let colorOrder=["red","blue","green","black"];
 let createTaskPriority="black";
 var uid = new ShortUniqueId();
 let box_visible=true;
 let deleteFlag=false;
+
+
+
 
 textarea_cont.addEventListener("keydown",function(e){
     if(e.key=="Enter"){
@@ -34,24 +38,22 @@ for(let j=0;j<priority_btn.length;j++){
     })
 }
 function createTask(task,createTaskPriority){
+    if (task=="") {
+        return;
+    }
     let ticket_body=document.createElement("div");
     ticket_body.setAttribute("class","ticket-body");
     ticket_body.innerHTML=`
             <div class="ticket-color ${createTaskPriority}"></div>
             <div class="ticket-id">#${uid()}</div>
-            <div class="ticket-desc">${task}</div>`;
+            <div class="ticket-desc">${task}</div>
+            <div class="lock-unlock"><i class="fa-solid fa-lock"></i></div>`;
     main_cont.appendChild(ticket_body);
 
-    //remove functionality
-    ticket_body.addEventListener("click",function(){
-        
-            if(deleteFlag){
-                ticket_body.remove();
-            }
-        add_btn.style.color="";
-
-    })
-
+    handleDelete(ticket_body);
+    handleLockUnlock(ticket_body);
+    handlePriorityColorChange(ticket_body);
+    
 }
 delete_btn.addEventListener("click",function(){
     if(deleteFlag){
@@ -75,4 +77,59 @@ add_btn.addEventListener("click",function(){
     box_visible=!box_visible;
 
 })
+
+function handleLockUnlock(ticket_body) {
+    // handle lock unlock
+    let lockUnlockBtn=ticket_body.querySelector(".lock-unlock i");
+    lockUnlockBtn.addEventListener("click",function(){
+        let ticket_desc= ticket_body.querySelector(".ticket-desc");
+        if(lockUnlockBtn.classList.contains('fa-lock')){
+            ticket_desc.setAttribute("contenteditable","true");
+            lockUnlockBtn.classList.remove("fa-lock"); 
+            lockUnlockBtn.classList.add("fa-lock-open");
+        }else{
+            lockUnlockBtn.classList.remove("fa-lock-open");
+            lockUnlockBtn.classList.add("fa-lock"); 
+            ticket_desc.setAttribute("contenteditable","false");
+        }
+    })
+}
+function handleDelete(ticket_body){
+        //remove functionality
+        ticket_body.addEventListener("click",function(){
+        
+            if(deleteFlag){
+                ticket_body.remove();
+            }
+        add_btn.style.color="";
+    })
+
+}
+function handlePriorityColorChange(ticket_body){
+    //handle change of priority color
+    let ticket_color=ticket_body.querySelector(".ticket-color");
+    // let colorOrder=["red","blue","green","black"];
+    ticket_color.addEventListener("click",function(){
+        let currentColor= ticket_color.classList[1];
+        // let currentColorIndex=-1;
+        // for (let index = 0; index < colorOrder.length; index++) {
+        //     const element = colorOrder[index];
+        //     if(element==currentColor)
+        //     currentColorIndex=index;
+        //     console.log(currentColorIndex);
+        // }
+        let currentColorIndex=colorOrder.findIndex(function (col) {
+            return currentColor==col;
+        })
+        console.log(currentColorIndex);
+        console.log(currentColor);
+        let nextColorIndex=(currentColorIndex +1)%colorOrder.length;
+        console.log(nextColorIndex);
+        ticket_color.classList.remove(currentColor);
+        ticket_color.classList.add(colorOrder[nextColorIndex]);
+
+
+    })
+}
+
 
